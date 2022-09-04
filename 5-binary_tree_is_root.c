19499cc6a73782050e538931e2facb616e5c6259
root <ali1998dehissy@gmail.com>
2022-09-04 14:26:50 -0700

    update

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -3,7 +3,6 @@
 #include <stdlib.h>
 /**
  * binary_tree_is_root - Function that checks if a given node is a root
- *
  * @node: Pointer to the node to check
  * Return: 1 if node is a root, otherwise 0
  */
